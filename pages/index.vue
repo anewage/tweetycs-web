bd036d277530503537768d9a5ccd60aa6ae56ee0
Amir Haghighati <haghighati.amir@gmail.com>
2019-05-21 18:37:45 -0400

    add sample Text Changer animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline"
           >Welcome to the Vuetify + Nuxt.js template</v-card-title
@@ -58,13 +54,7 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  components: {}
 }
 </script>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,16 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+      <text-changer :id="'text-changer'" />
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import TextChanger from '../components/TextChanger'
+export default {
+  components: {
+    'text-changer': TextChanger
+  }
+}
+</script>
